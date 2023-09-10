7bfc75be1c4f05030d941ebb50b686b5ceb1e3f9
Tejas Lamba <tejas22feb@gmail.com>
2023-09-10 22:26:01 +0530

    help taken from katdeesea

--- index.js
+++ index.js
@@ -6,19 +6,21 @@
  * modified or distributed without permission. For more information,
  * dm the author on discord.
  */
-import * as config from './config.js';
-import * as logger from './src/logger.js';
-import express from 'express';
-import { Client, GatewayIntentBits, Options } from 'discord.js';
-import 'dotenv/config';
+import * as config from "./config.js";
+import * as logger from "./src/logger.js";
+import express from "express";
+import { Client, GatewayIntentBits, Options } from "discord.js";
+import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send({ error: false, message: 'Ticket Bot is online!' }));
+app.get("/", (req, res) =>
+  res.send({ error: false, message: "Ticket Bot is online!" })
+);
 app.listen(port, () => console.log(`Web Server Listening on port ${port}!`));
 
-const client = new Client({
+export const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
@@ -40,11 +42,9 @@ const client = new Client({
 client.config = config;
 client.logger = logger;
 
-process.on('unhandledRejection', (error) => console.log(error, 'error'));
-process.on('uncaughtException', (error) => console.log(error, 'error'));
-
+process.on("unhandledRejection", (error) => console.log(error, "error"));
+process.on("uncaughtException", (error) => console.log(error, "error"));
 
 client.login(process.env.token);
 
-export default client;
-import './src/handler.js';
+import "./src/handler.js";
--- src/handler.js
+++ src/handler.js
@@ -1,19 +1,21 @@
-import { readdirSync } from 'fs';
-import { Collection } from 'discord.js';
-import client from '../index.js';
+import { readdirSync } from "fs";
+import { Collection } from "discord.js";
+import { client } from "../index.js";
 
 const commands = new Collection();
 
 const loadEvents = async () => {
-  const events = readdirSync('./events/').filter(file => file.endsWith('.js'));
+  const events = readdirSync("./events/").filter((file) =>
+    file.endsWith(".js")
+  );
   client.logger.log(`Loading events...`, "event");
 
   for (const file of events) {
     try {
       const module = await import(`../events/${file}`);
       const event = module.default;
-      client.logger.log(`-> Loaded event ${file.split('.')[0]}`, "event");
-      client.on(file.split('.')[0], event.bind(null, client));
+      client.logger.log(`-> Loaded event ${file.split(".")[0]}`, "event");
+      client.on(file.split(".")[0], event.bind(null, client));
     } catch (error) {
       console.error(error);
     }
@@ -21,14 +23,19 @@ const loadEvents = async () => {
 };
 
 const loadCommands = async () => {
-  const commandFiles = readdirSync('./commands/').filter(file => file.endsWith('.js'));
+  const commandFiles = readdirSync("./commands/").filter((file) =>
+    file.endsWith(".js")
+  );
   client.logger.log(`Loading commands...`, "command");
 
   for (const file of commandFiles) {
     try {
       const module = await import(`../commands/${file}`);
       const command = module.default;
-      client.logger.log(`-> Loaded command ${command.name.toLowerCase()}`, "command");
+      client.logger.log(
+        `-> Loaded command ${command.name.toLowerCase()}`,
+        "command"
+      );
       commands.set(command.name.toLowerCase(), command);
     } catch (error) {
       console.error(error);
